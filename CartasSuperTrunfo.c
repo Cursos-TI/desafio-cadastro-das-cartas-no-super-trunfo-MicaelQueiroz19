901f33daa2e920a3e99a62aff7c5c5e96d285d00
MicaelQueiroz19 <micaelqgomes@gmail.com>
2025-02-08 14:58:18 +0000

    Cadastro e consultar de multiplas CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,58 +1,93 @@
 #include <stdio.h>
+#include <string.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
-    char carta[4] = "A01";
-    int populacao, pontos_turisticos;
-    float area, pib;
+// Sugestão: Defina variáveis separadas para cada atributo da cidade.
+// Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+int populacao, pontos_turisticos;
+float area, pib;
+char carta[4];
+char estados[3] = {'A','B'};
 
-    char estados[10] = {'A','B'};
-    for (int i = 0; i < 4; i++){
 
-        printf("Carta: %d\n", i+1);
-        printf("População: "); // Mensagem
-        scanf("%d", &populacao); // Solicita ao usuário que insira a população.
-        printf("Área: "); // Mensagem
-        scanf("%f", &area); // Solicita ao usuário que insira a área.
-        printf("PIB: "); // Mensagem
-        scanf("%f", &pib); // Solicita ao usuário que insira o PIB.
-        printf("Número de pontos turísticos: "); // Mensagem
-        scanf("%d", &pontos_turisticos); // Solicita ao usuário que insira os pontos turisticos.
+// Cadastro das Cartas:
+// Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+// Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+void cadastrar_cartas(){
+
+    // Abre aquivo .txt para salvar cartas
+    FILE *arquivo = fopen("dados.txt", "w");
+    if (arquivo==NULL) {
+        printf("Erro ao abrir o arquivo");
     }
     
+
+    for (int j = 0; j < 2; j++) { // 
+        for (int i = 0; i < 4; i++){
+            snprintf(carta, sizeof(carta), "%c%02d", estados[j], i+1);
+            printf("\nCarta: %s\n", carta);
+            printf("População: "); // Mensagem
+            scanf("%d", &populacao); // Solicita ao usuário que insira a população.
+            printf("Área: "); // Mensagem
+            scanf("%f", &area); // Solicita ao usuário que insira a área.
+            printf("PIB: "); // Mensagem
+            scanf("%f", &pib); // Solicita ao usuário que insira o PIB.
+            printf("Número de pontos turísticos: "); // Mensagem
+            scanf("%d", &pontos_turisticos); // Solicita ao usuário que insira os pontos turisticos.
+
+            // Escrever no arquivo
+            fprintf(arquivo, "Carta: %s\n", carta);
+            fprintf(arquivo, "População: %d\n", populacao);
+            fprintf(arquivo, "Área: %.2f\n", area);
+            fprintf(arquivo, "PIB: %.2f\n", pib);
+            fprintf(arquivo, "Número de pontos turísticos: %d\n\n", pontos_turisticos);
+        }
+    }
+
+    fclose(arquivo);
+    printf("Cartas Salvas!\n\n");
+}
+
+// Exibição dos Dados das Cartas:
+// Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+// Exiba os valores inseridos para cada atributo da cidade, um por linha.
+void consultar_cartas(){
+    // Abre arquivo dados.
+    FILE *arquivo = fopen("dados.txt", "r");
+
+    // Verificar se o arquivo foi aberto corretamente.
+    if (arquivo == NULL) {
+        printf("Erro: O arquivo não pôde ser aberto!\n");
+    }
+    // Lê o conteúdo do arquivo linha por linha
+    char linha[100]; // buffer para armazenar cada linha.
+    while (fgets(linha,sizeof(linha), arquivo) != NULL) {
+        printf("%s", linha); // Imprime cada linha lida do arquivo
+    }
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+}
 
 
-    //printf("População: \n"); // Mensagem
-    //scanf("%d", &populacao); // Solicita ao usuário que insira a população.
-    //printf("Área: \n"); // Mensagem
-    //scanf("%f", &area); // Solicita ao usuário que insira a área.
-    //printf("PIB: \n"); // Mensagem
-    //scanf("%f", &pib); // Solicita ao usuário que insira o PIB.
-    //printf("Número de pontos turísticos: \n"); // Mensagem
-    //scanf("%d", &pontos_turisticos); // Solicita ao usuário que insira os pontos turisticos.
+int main() {
+    char escolha[20];
 
+    printf("Bem Vindo ao Jogo Super Trunfo (Tema: Paises)\n");
+    printf("O que deseja fazer com as Cartas? (Consultar/Cadastrar):");
+    scanf("%s", escolha);
 
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    if (strcmp(escolha, "Cadastrar") == 0){
+        cadastrar_cartas();
+    } else if (strcmp(escolha, "Consultar") == 0){
+        consultar_cartas();
+    } else {
+        printf("Opção inválida! Reinicie o Jogo e escolha entre 'Cadastrar' ou 'Consultar'.\n");
+    }
 
-    printf("Carta: %s\n", carta);
-    printf("População: %d\n", populacao);
-    printf("Área: %.2f\n", area);
-    printf("PIB: %.2f\n", pib);
-    printf("Pontos: %d\n", pontos_turisticos);
 
     return 0;
 }
