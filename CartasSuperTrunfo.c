a63d28a8472e19c4e89937042d1b04e2852b7b93
MicaelQueiroz19 <micaelqgomes@gmail.com>
2025-02-13 01:15:35 +0000

    Cadastrar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,7 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 
-// Sugestão: Defina variáveis separadas para cada atributo da cidade.
-// Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+// Define variáveis separadas para cada atributo da cidade.
 char estado[20];
 char codigoCidade[20];
 char nomeCidade[20];
@@ -16,35 +15,35 @@ float area;
 float pib;
 int pontosturisticos;
 
-
 // Cadastro das Cartas:
-// Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-// Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 void cadastrarCartas(){
     printf("Cadastrar a Cidade.\n\n");
+    char letra = 65; // Contador para sequência de letras de codigoCidade (65 é a referência a letra A da tabela ASCII).
+    int contador = 1; // Contador para sequencia de números de codigoCidade.
 
-    int contador = 0;
-    while (contador < 4) {
-        contador ++;
-        printf("Estado: ");
-        fgets(estado, sizeof(estado), stdin);
-        printf("Código da Cidade: %s", codigoCidade);
-        printf("Nome da Cidade: ");
-        fgets(nomeCidade, sizeof(nomeCidade), stdin);
-        printf("População: ");
-        scanf("%d", &populacao);
-        printf("Área em km²: ");
-        scanf("%f", &area);
-        printf("PIB: ");
-        scanf("%f", &pib);
-        printf("Número de pontos turísticos: ");
-        scanf("%d", &pontosturisticos);
-
-        printf("-- Carta Cadastrada! --\n\n");
-        getchar();
-        
+    // Os 2 while serve para estruturar a sequencia de A01 até H04.
+    while (letra < 73) { // verifica se a letra da variável é < "I". (Conta de A até H).
+        while (contador < 5) { // (Conta de 1 até 4).
+            sprintf(codigoCidade, "%c0%d", letra, contador); // Concatena letra e contador.
+            printf("Estado: ");
+            fgets(estado, sizeof(estado), stdin);
+            printf("Código da Cidade: %s\n", codigoCidade);
+            printf("Nome da Cidade: ");
+            fgets(nomeCidade, sizeof(nomeCidade), stdin);
+            printf("População: ");
+            scanf("%d", &populacao);
+            printf("Área em km²: ");
+            scanf("%f", &area);
+            printf("PIB: ");
+            scanf("%f", &pib);
+            printf("Número de pontos turísticos: ");
+            scanf("%d", &pontosturisticos);
+            printf("-- Carta Cadastrada! --\n\n");
+            getchar();
+            contador ++;  // Adiciona mais 1 na contagem
+        }
+    letra++; // Adiciona mais 1 letra na sequência.
     }
-
 }
 
 
@@ -53,8 +52,11 @@ void cadastrarCartas(){
 // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
 
+
 int main() {
 
-    cadastrarCartas();
+    //cadastrarCartas();
+
     
 }
+
